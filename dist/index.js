524bff8e155cdbc3df453214b45f7ae64e6ba6cd
Kartik Khorwal <hellokartikk@gmail.com>
2024-01-26 14:09:15 +0530

    update main function

--- dist/index.js
+++ dist/index.js
@@ -4,24 +4,26 @@
   new EventSource(`${"http://localhost:3000"}/esbuild`).addEventListener("change", () => location.reload());
 
   // src/utils/greenhouse.ts
-  var results_per_page = 3;
-  var current_page = 1;
-  var allData = [];
-  var dataStore;
-  var currentData = [];
-  var filters = [{
-    name: "department",
-    options: ["All Departments"]
-  }, {
-    name: "location",
-    options: ["All Locations"]
-  }];
-  var mainElement;
-  var list;
-  var listElement;
-  var searchElement;
-  var REQUIRED_FIELDS = ["department", "title", "location", "content"];
   var greenhouse = async () => {
+    let results_per_page = 3;
+    let current_page = 1;
+    let apiData = [];
+    let dataStore = [];
+    let filteredData;
+    let paginatedData = [];
+    let filters = [{
+      name: "department",
+      options: ["All Departments"]
+    }, {
+      name: "location",
+      options: ["All Locations"]
+    }];
+    let mainElement;
+    let list;
+    let listElement;
+    let searchElement;
+    let paginate = false;
+    let REQUIRED_FIELDS = ["department", "title", "location", "content"];
     mainElement = document.querySelectorAll('[tc-greenhouse-element="main"]')[0];
     list = mainElement?.querySelectorAll('[tc-greenhouse-element="list"]')[0];
     listElement = list?.querySelectorAll('[tc-greenhouse-element="list-item"]')[0];
@@ -35,139 +37,140 @@
     previousButton.addEventListener("click", handlePrevious);
     await getDataFromGreenhouseAPI();
     setFilters();
-  };
-  function renderList() {
-    if (!listElement)
-      return;
-    let items = [];
-    currentData.forEach((item) => {
-      let newElement = listElement.cloneNode(true);
-      newElement.style.display = "flex";
-      newElement.style.opacity = "1";
-      REQUIRED_FIELDS.forEach((field) => {
-        newElement.querySelectorAll(`[tc-greenhouse-element="${field}"]`).forEach((element) => {
-          if (field === "location") {
-            element.innerHTML = item.location.name;
-          } else {
-            element.textContent = item[field];
-          }
+    function renderList() {
+      if (!listElement)
+        return;
+      let items = [];
+      paginatedData.forEach((item) => {
+        let newElement = listElement.cloneNode(true);
+        newElement.style.display = "flex";
+        newElement.style.opacity = "1";
+        REQUIRED_FIELDS.forEach((field) => {
+          newElement.querySelectorAll(`[tc-greenhouse-element="${field}"]`).forEach((element) => {
+            if (field === "location") {
+              element.innerHTML = item.location.name;
+            } else {
+              element.textContent = item[field];
+            }
+          });
         });
+        items.push(newElement);
       });
-      items.push(newElement);
-    });
-    list.innerHTML = "";
-    list?.append(...items);
-  }
-  function setCurrentPageData() {
-    if (!dataStore)
-      return;
-    console.log("current_page", current_page);
-    currentData = dataStore.slice((current_page - 1) * results_per_page, current_page * results_per_page);
-    console.log("currentData", currentData);
-    renderList();
-  }
-  function handleInputChange(value) {
-    if (!dataStore)
-      return;
-    console.log("hello");
-    if (!value.trim()) {
-      dataStore = allData;
-    } else {
-      dataStore = dataStore.filter((item) => {
-        return item.title.toLowerCase().includes(value.toLowerCase());
-      });
+      list.innerHTML = "";
+      list?.append(...items);
     }
-    setCurrentPageData();
-  }
-  function handlePrevious() {
-    if (current_page > 1) {
-      current_page--;
-      currentData = dataStore.slice((current_page - 1) * results_per_page, current_page * results_per_page);
+    function setCurrentPageData() {
+      if (!filteredData)
+        return;
+      console.log("current_page", current_page);
+      paginatedData = filteredData.slice((current_page - 1) * results_per_page, current_page * results_per_page);
+      console.log("paginatedData", paginatedData);
       renderList();
     }
-    list?.scrollIntoView({ behavior: "smooth" });
-  }
-  function handleNext() {
-    if (current_page < Math.ceil(dataStore.length / results_per_page)) {
-      current_page++;
-      currentData = dataStore.slice((current_page - 1) * results_per_page, current_page * results_per_page);
-      renderList();
-    }
-    list?.scrollIntoView({ behavior: "smooth" });
-  }
-  function setFilters() {
-    let filter_options = filters.map((filter) => filter.name);
-    allData.forEach((item) => {
-      let existing_department = filters[0].options.map((option) => option);
-      let existing_location = filters[1].options.map((option) => option);
-      let department = item.departments[0].name;
-      let location2 = item.location.name;
-      if (!existing_department.includes(department))
-        filters[0].options.push(item.departments[0].name);
-      if (!existing_location.includes(location2))
-        filters[1].options.push(item.location.name);
-    });
-    let filterElements = mainElement?.querySelectorAll("[tc-greenhouse-filter]");
-    console.log(filterElements, "filterElements");
-    if (filterElements?.length)
-      filterElements.forEach((item) => {
-        let filter_type = item.getAttribute("tc-greenhouse-filter");
-        if (filter_options.includes(filter_type)) {
-          let options = filters.find((filter) => filter.name === filter_type)?.options;
-          if (options?.length) {
-            options.forEach((option) => {
-              let newOption = document.createElement("option");
-              newOption.value = option;
-              newOption.textContent = option;
-              item.appendChild(newOption);
-            });
-          }
-        }
-        item.onchange = (e) => {
-          handleFilterChange(e);
-        };
-      });
-  }
-  function handleFilterChange(e) {
-    const locationFilter = document.querySelector('[tc-greenhouse-filter="location"]');
-    const departmentFilter = document.querySelector('[tc-greenhouse-filter="department"]');
-    let filteredData = [];
-    if (locationFilter?.value && departmentFilter?.value) {
-      if (locationFilter.value === "All Locations" && departmentFilter.value === "All Departments")
-        filteredData = allData;
-      else if (locationFilter.value === "All Locations") {
-        allData.forEach((item) => {
-          if (item.departments[0].name === departmentFilter.value) {
-            filteredData.push(item);
-          }
-        });
-      } else if (departmentFilter.value === "All Departments") {
-        allData.forEach((item) => {
-          if (item.location.name === locationFilter.value) {
-            filteredData.push(item);
-          }
-        });
+    function handleInputChange(value) {
+      if (!filteredData)
+        return;
+      console.log(value.trim());
+      if (!value.trim()) {
+        filteredData = apiData;
       } else {
-        allData.forEach((item) => {
-          if (item.departments[0].name === departmentFilter.value && item.location.name === locationFilter.value) {
-            filteredData.push(item);
-          }
+        filteredData = filteredData.filter((item) => {
+          console.log(item.title.trim().toLowerCase().includes(value.trim().toLowerCase()));
+          return item.title.trim().toLowerCase().includes(value.trim().toLowerCase());
         });
       }
+      setCurrentPageData();
     }
-    console.log(filteredData.length);
-    dataStore = filteredData;
-    current_page = 1;
-    setCurrentPageData();
-  }
-  async function getDataFromGreenhouseAPI() {
-    let res = await fetch(`https://boards-api.greenhouse.io/v1/boards/mural/jobs?content=true`, {
-      method: "GET"
-    }).then((res2) => res2.json()).catch((err) => console.log(err));
-    allData = res.jobs;
-    dataStore = res.jobs;
-    setCurrentPageData();
-  }
+    function handlePrevious() {
+      if (current_page > 1) {
+        current_page--;
+        paginatedData = filteredData.slice((current_page - 1) * results_per_page, current_page * results_per_page);
+        renderList();
+      }
+      list?.scrollIntoView({ behavior: "smooth" });
+    }
+    function handleNext() {
+      if (current_page < Math.ceil(filteredData.length / results_per_page)) {
+        current_page++;
+        paginatedData = filteredData.slice((current_page - 1) * results_per_page, current_page * results_per_page);
+        renderList();
+      }
+      list?.scrollIntoView({ behavior: "smooth" });
+    }
+    function setFilters() {
+      let filter_options = filters.map((filter) => filter.name);
+      apiData.forEach((item) => {
+        let existing_department = filters[0].options.map((option) => option);
+        let existing_location = filters[1].options.map((option) => option);
+        let department = item.departments[0].name;
+        let location2 = item.location.name;
+        if (!existing_department.includes(department))
+          filters[0].options.push(item.departments[0].name);
+        if (!existing_location.includes(location2))
+          filters[1].options.push(item.location.name);
+      });
+      let filterElements = mainElement?.querySelectorAll("[tc-greenhouse-filter]");
+      console.log(filterElements, "filterElements");
+      if (filterElements?.length)
+        filterElements.forEach((item) => {
+          let filter_type = item.getAttribute("tc-greenhouse-filter");
+          if (filter_options.includes(filter_type)) {
+            let options = filters.find((filter) => filter.name === filter_type)?.options;
+            if (options?.length) {
+              options.forEach((option) => {
+                let newOption = document.createElement("option");
+                newOption.value = option;
+                newOption.textContent = option;
+                item.appendChild(newOption);
+              });
+            }
+          }
+          item.onchange = (e) => {
+            handleFilterChange(e);
+          };
+        });
+    }
+    function handleFilterChange(e) {
+      const locationFilter = document.querySelector('[tc-greenhouse-filter="location"]');
+      const departmentFilter = document.querySelector('[tc-greenhouse-filter="department"]');
+      let filteredData2 = [];
+      if (locationFilter?.value && departmentFilter?.value) {
+        if (locationFilter.value === "All Locations" && departmentFilter.value === "All Departments")
+          filteredData2 = apiData;
+        else if (locationFilter.value === "All Locations") {
+          apiData.forEach((item) => {
+            if (item.departments[0].name === departmentFilter.value) {
+              filteredData2.push(item);
+            }
+          });
+        } else if (departmentFilter.value === "All Departments") {
+          apiData.forEach((item) => {
+            if (item.location.name === locationFilter.value) {
+              filteredData2.push(item);
+            }
+          });
+        } else {
+          apiData.forEach((item) => {
+            if (item.departments[0].name === departmentFilter.value && item.location.name === locationFilter.value) {
+              filteredData2.push(item);
+            }
+          });
+        }
+      }
+      console.log(filteredData2.length);
+      filteredData2 = filteredData2;
+      current_page = 1;
+      setCurrentPageData();
+    }
+    async function getDataFromGreenhouseAPI() {
+      let res = await fetch(`https://boards-api.greenhouse.io/v1/boards/mural/jobs?content=true`, {
+        method: "GET"
+      }).then((res2) => res2.json()).catch((err) => console.log(err));
+      apiData = res.jobs;
+      filteredData = res.jobs;
+      setCurrentPageData();
+    }
+  };
 
   // src/index.ts
   window.Webflow ||= [];
--- src/utils/greenhouse.ts
+++ src/utils/greenhouse.ts
@@ -1,29 +1,36 @@
 
-let results_per_page = 3;
-let current_page = 1;
-
-let allData: job[] = [] // all data from api response
-let dataStore: undefined | null | job[] // all data with filters/search applied
-
-let currentData: job[] = [] // paginated data
-
-let filters: filter[] = [{
-  name: 'department',
-  options: ['All Departments']
-}, {
-  name: 'location',
-  options: ['All Locations']
-}]
-
-// components required
-let mainElement: Element | undefined
-let list: Element | undefined
-let listElement: Element | undefined
-let searchElement: HTMLInputElement | undefined
-
-let REQUIRED_FIELDS = ['department', 'title', 'location', 'content']
 
 export const greenhouse = async () => {
+  let results_per_page = 3;
+  let current_page = 1;
+
+  let apiData: job[] = [] // all data from api response
+  let dataStore: job[] = []
+  let filteredData: undefined | null | job[] // all data with filters/search applied
+
+  let paginatedData: job[] = [] // paginated data
+
+  let filters: filter[] = [{
+    name: 'department',
+    options: ['All Departments']
+  }, {
+    name: 'location',
+    options: ['All Locations']
+  }]
+
+  // components required
+  let mainElement: Element | undefined
+  let list: Element | undefined
+  let listElement: Element | undefined
+  let searchElement: HTMLInputElement | undefined
+
+  let paginate: boolean = false
+
+  let REQUIRED_FIELDS = ['department', 'title', 'location', 'content']
+
+
+  // settings
+  // paginate = 
 
   // Getting all the required elements
   mainElement = document.querySelectorAll('[tc-greenhouse-element="main"]')[0];
@@ -46,165 +53,170 @@ export const greenhouse = async () => {
 
   await getDataFromGreenhouseAPI()
   setFilters()
-};
+  
+  function renderList() {
+    if (!listElement) return
 
-function renderList() {
-  if (!listElement) return
+    let items: HTMLElement[] = []
 
-  let items: HTMLElement[] = []
+    // creating list of elements
+    paginatedData.forEach(item => {
+      let newElement = listElement!.cloneNode(true) as HTMLElement
 
-  // creating list of elements
-  currentData.forEach(item => {
-    let newElement = listElement!.cloneNode(true) as HTMLElement
-
-    newElement.style.display = 'flex'
-    newElement.style.opacity = '1'
-    REQUIRED_FIELDS.forEach(field => {
-      // find all elements of the current field
-      newElement.querySelectorAll(`[tc-greenhouse-element="${field}"]`).forEach(element => {
-        // replacing with actual data
-        if (field === 'location') {
-          element.innerHTML = item.location.name
-        } else {
-          element.textContent = item[field as keyof job]
-        }
+      newElement.style.display = 'flex'
+      newElement.style.opacity = '1'
+      REQUIRED_FIELDS.forEach(field => {
+        // find all elements of the current field
+        newElement.querySelectorAll(`[tc-greenhouse-element="${field}"]`).forEach(element => {
+          // replacing with actual data
+          if (field === 'location') {
+            element.innerHTML = item.location.name
+          } else {
+            element.textContent = item[field as keyof job]
+          }
+        })
       })
+      items.push(newElement)
     })
-    items.push(newElement)
-  })
 
-  // clearing current list of elements
-  list!.innerHTML = ''
-  list?.append(...items)
-}
-
-function setCurrentPageData() {
-  if (!dataStore) return
-  console.log("current_page", current_page)
-  currentData = dataStore.slice((current_page - 1) * results_per_page, current_page * results_per_page)
-  console.log("currentData", currentData)
-  renderList()
-}
-
-function handleInputChange(value: string) {
-  if (!dataStore) return
-  console.log("hello")
-  if (!value.trim()) {
-    dataStore = allData
-  } else {
-    dataStore = dataStore!.filter(item => {
-      return item.title.toLowerCase().includes(value.toLowerCase())
-      // ||
-      //   item.content.toLowerCase().includes(value.toLowerCase())
-    })
+    // clearing current list of elements
+    list!.innerHTML = ''
+    list?.append(...items)
   }
-  setCurrentPageData()
-}
 
-function handlePrevious() {
-  if (current_page > 1) {
-    current_page--
-    currentData = dataStore!.slice((current_page - 1) * results_per_page, current_page * results_per_page)
+  function setCurrentPageData() {
+    if (!filteredData) return
+    console.log("current_page", current_page)
+    paginatedData = filteredData.slice((current_page - 1) * results_per_page, current_page * results_per_page)
+    console.log("paginatedData", paginatedData)
     renderList()
   }
-  list?.scrollIntoView({ behavior: 'smooth' })
-}
 
-function handleNext() {
-  if (current_page < Math.ceil(dataStore!.length / results_per_page)) {
-    current_page++
-    currentData = dataStore!.slice((current_page - 1) * results_per_page, current_page * results_per_page)
-    renderList()
+  function handleInputChange(value: string) {
+    if (!filteredData) return
+    console.log(value.trim())
+    if (!value.trim()) {
+      filteredData = apiData
+    } else {
+      filteredData = filteredData.filter(item => {
+        console.log(item.title.trim().toLowerCase().includes(value.trim().toLowerCase()))
+        return item.title.trim().toLowerCase().includes(value.trim().toLowerCase())
+        // ||
+        //   item.content.toLowerCase().includes(value.toLowerCase())
+      })
+    }
+    setCurrentPageData()
   }
-  list?.scrollIntoView({ behavior: 'smooth' })
-}
 
-function setFilters() {
-  let filter_options = filters.map(filter => filter.name)
+  function handlePrevious() {
+    if (current_page > 1) {
+      current_page--
+      paginatedData = filteredData!.slice((current_page - 1) * results_per_page, current_page * results_per_page)
+      renderList()
+    }
+    list?.scrollIntoView({ behavior: 'smooth' })
+  }
 
-  // set filters
-  allData.forEach(item => {
-    let existing_department = filters[0].options.map(option => option)
-    let existing_location = filters[1].options.map(option => option)
+  function handleNext() {
+    if (current_page < Math.ceil(filteredData!.length / results_per_page)) {
+      current_page++
+      paginatedData = filteredData!.slice((current_page - 1) * results_per_page, current_page * results_per_page)
+      renderList()
+    }
+    list?.scrollIntoView({ behavior: 'smooth' })
+  }
 
-    // department
-    let department = item.departments[0].name
-    let location = item.location.name
+  function setFilters() {
+    let filter_options = filters.map(filter => filter.name)
 
-    // department
-    if (!existing_department.includes(department)) filters[0].options.push(item.departments[0].name)
-    // location
-    if (!existing_location.includes(location)) filters[1].options.push(item.location.name)
-  })
+    // set filters
+    apiData.forEach(item => {
+      let existing_department = filters[0].options.map(option => option)
+      let existing_location = filters[1].options.map(option => option)
 
-  let filterElements = mainElement?.querySelectorAll('[tc-greenhouse-filter]') as NodeListOf<HTMLSelectElement>
-  console.log(filterElements, "filterElements")
+      // department
+      let department = item.departments[0].name
+      let location = item.location.name
 
-  if (filterElements?.length) filterElements.forEach(item => {
+      // department
+      if (!existing_department.includes(department)) filters[0].options.push(item.departments[0].name)
+      // location
+      if (!existing_location.includes(location)) filters[1].options.push(item.location.name)
+    })
 
-    let filter_type = item.getAttribute('tc-greenhouse-filter') as string
+    let filterElements = mainElement?.querySelectorAll('[tc-greenhouse-filter]') as NodeListOf<HTMLSelectElement>
+    console.log(filterElements, "filterElements")
 
-    if (filter_options.includes(filter_type)) { // check if valid filter
-      let options = filters.find(filter => filter.name === filter_type)?.options
-      if (options?.length) {
-        options.forEach(option => {
-          let newOption = document.createElement('option')
-          newOption.value = option
-          newOption.textContent = option
-          item.appendChild(newOption)
+    if (filterElements?.length) filterElements.forEach(item => {
+
+      let filter_type = item.getAttribute('tc-greenhouse-filter') as string
+
+      if (filter_options.includes(filter_type)) { // check if valid filter
+        let options = filters.find(filter => filter.name === filter_type)?.options
+        if (options?.length) {
+          options.forEach(option => {
+            let newOption = document.createElement('option')
+            newOption.value = option
+            newOption.textContent = option
+            item.appendChild(newOption)
+          })
+        }
+      }
+
+      item.onchange = (e) => { handleFilterChange(e) }
+    })
+
+
+  }
+
+  function handleFilterChange(e: Event) {
+
+    const locationFilter = document.querySelector('[tc-greenhouse-filter="location"]') as HTMLSelectElement
+    const departmentFilter = document.querySelector('[tc-greenhouse-filter="department"]') as HTMLSelectElement
+
+    let filteredData: job[] = []
+
+    if (locationFilter?.value && departmentFilter?.value) {
+
+      if (locationFilter.value === 'All Locations' && departmentFilter.value === 'All Departments') filteredData = apiData
+      else if (locationFilter.value === 'All Locations') {
+        apiData.forEach(item => {
+          if (item.departments[0].name === departmentFilter.value) {
+            filteredData.push(item)
+          }
+        })
+      } else if (departmentFilter.value === 'All Departments') {
+        apiData.forEach(item => {
+          if (item.location.name === locationFilter.value) {
+            filteredData.push(item)
+          }
+        })
+      } else {
+        apiData.forEach(item => {
+          if (item.departments[0].name === departmentFilter.value && item.location.name === locationFilter.value) {
+            filteredData.push(item)
+          }
         })
       }
     }
-
-    item.onchange = (e) => { handleFilterChange(e) }
-  })
-
-
-}
-
-function handleFilterChange(e: Event) {
-
-  const locationFilter = document.querySelector('[tc-greenhouse-filter="location"]') as HTMLSelectElement
-  const departmentFilter = document.querySelector('[tc-greenhouse-filter="department"]') as HTMLSelectElement
-
-  let filteredData: job[] = []
-
-  if (locationFilter?.value && departmentFilter?.value) {
-
-    if (locationFilter.value === 'All Locations' && departmentFilter.value === 'All Departments') filteredData = allData
-    else if (locationFilter.value === 'All Locations') {
-      allData.forEach(item => {
-        if (item.departments[0].name === departmentFilter.value) {
-          filteredData.push(item)
-        }
-      })
-    } else if (departmentFilter.value === 'All Departments') {
-      allData.forEach(item => {
-        if (item.location.name === locationFilter.value) {
-          filteredData.push(item)
-        }
-      })
-    } else {
-      allData.forEach(item => {
-        if (item.departments[0].name === departmentFilter.value && item.location.name === locationFilter.value) {
-          filteredData.push(item)
-        }
-      })
-    }
+    console.log(filteredData.length)
+    filteredData = filteredData
+    current_page = 1
+    setCurrentPageData()
   }
-  console.log(filteredData.length)
-  dataStore = filteredData
-  current_page = 1
-  setCurrentPageData()
-}
 
 
-async function getDataFromGreenhouseAPI() {
-  let res = await fetch(`https://boards-api.greenhouse.io/v1/boards/mural/jobs?content=true`, {
-    method: 'GET',
-  }).then(res => res.json()).catch(err => console.log(err))
+  async function getDataFromGreenhouseAPI() {
 
-  allData = res.jobs
-  dataStore = res.jobs
-  setCurrentPageData()
-}
+    // fake await promise
+    // await new Promise(resolve => setTimeout(resolve, 5000)); // waits for 2 seconds
+
+    let res = await fetch(`https://boards-api.greenhouse.io/v1/boards/mural/jobs?content=true`, {
+      method: 'GET',
+    }).then(res => res.json()).catch(err => console.log(err))
+
+    apiData = res.jobs
+    filteredData = res.jobs
+    setCurrentPageData()
+  }
+};
